98dcee8b1debaccb2bb200df911ab6a794fda83b
Gali Geller <gali1998@gmail.com>
2020-12-20 08:36:11 +0200

    App.js has search experimented component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, {Component} from 'react';
 import './App.css';
+import ToolbarComponent from './Components/ToolbarComponent/ToolbarComponent';
+import SearchExperimenterComponent from './Components/SearchExperimenterComponent/SearchExperimenterComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ToolbarComponent className="item"></ToolbarComponent>
+        <SearchExperimenterComponent className="item"></SearchExperimenterComponent>
+      </div>
+    );
+  }
 }
 
 export default App;
