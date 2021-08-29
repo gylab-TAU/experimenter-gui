6bf45d4c69cb26209deaa88a0d1562f09e01372a
Gali <galigeller@mail.tau.ac.il>
2021-08-29 21:07:44 +0300

    update port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port =  process.env.PORT || 1339; 
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -8,4 +10,4 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(1339);
+app.listen(port);
